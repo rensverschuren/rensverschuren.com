30921d83ab5f2bb527f04d2e541f0e069f7538dd
Rens <hi@rensverschuren.com>
2019-07-02 09:49:29 +0200

    Update working place

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Rens Verschuren" keywords={[`Rens`, `Verschuren`, `Developer`, `Netherlands`, `Castual`]} />
     <Container>
-      <p>Hi, my name is Rens Verschuren and I’m a web developer from the Netherlands. At the moment I’m specialised in React and Ruby on Rails. I have a keen eye for clean interfaces and love to build things that are a joy to use.</p>
-      <p>At the moment I’m working on <a href="http://castual.com" rel="noopener noreferrer" target="_blank">Castual</a>: an app that makes it easy to create beautiful digital signage.</p>
+      <p>Hi, my name is Rens Verschuren and I’m a developer from the Netherlands. At the moment I’m working at <a href="https://bakkenbaeck.com/" target="_blank">Bakken & Bæck</a>. I have a keen eye for clean interfaces and love to build things that are a joy to use.</p>
+      <p>I’m also working on <a href="http://castual.com" rel="noopener noreferrer" target="_blank">Castual</a>: an app that makes it easy to create beautiful digital signage.</p>
       <p>In my spare time I love to play the saxophone and read interesting books.</p>
       <p>Say <a href="mailto:hi@rensverschuren.com">hi@rensverschuren.com</a></p>
     </Container>
